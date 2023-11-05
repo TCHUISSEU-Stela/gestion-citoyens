dd371e31b8ae9b080f0eb536401cf85d10747f31
jiogho <jioghoj@gmail.com>
2023-11-05 18:36:31 +0100

    mie a jour due header

--- src/app/core/layouts/header/header.component.ts
+++ src/app/core/layouts/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+     selector: 'app-header',
+     templateUrl: './header.component.html',
+     styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+     isToggleSidebar!: boolean;
 
+     ngOnInit(): void {
+          this.isToggleSidebar = false;
+     }
+
+     onToggleSidebar() {
+          this.isToggleSidebar ? (this.isToggleSidebar = false) : (this.isToggleSidebar = true);
+     }
 }
