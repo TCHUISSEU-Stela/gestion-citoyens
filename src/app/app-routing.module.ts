b1bda851eb3531c184c1dc82fd4231a63c679334
jiogho <jioghoj@gmail.com>
2023-11-06 12:42:51 +0100

    integration  du routerOutlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+     { path: '', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
+     { path: 'auth', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
+     { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+     { path: 'citoyens', loadChildren: () => import('./citoyens/citoyens.module').then(m => m.CitoyensModule) },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+     imports: [RouterModule.forRoot(routes)],
+     exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+     { path: 'login', component: LoginComponent, title: 'login' },
+     { path: 'register', component: RegisterComponent, title: 'register' },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+     imports: [RouterModule.forChild(routes)],
+     exports: [RouterModule],
 })
-export class CoreRoutingModule { }
+export class CoreRoutingModule {}
